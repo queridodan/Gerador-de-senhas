f6b9b890f6841ad50e81e56bb762d37305abed3e
DANIEL  RIBEIRO <dribeirofotografia@hotmail.com>
2022-04-21 18:22:55 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let conteinerPassword = document.querySelector("#conteiner-password");
+
+let charset = "abcdefghijklmnopqrstuvxzywABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@"
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+sliderElement.oninput = function(){
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword(){
+  
+    let pass = "";
+    for (let i = 0, n = charset.length; i < sliderElement.value; ++i) {
+        pass += charset.charAt(Math.floor(Math.random() * n))
+    }
+     
+    conteinerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha = pass;
+
+}
+
+function copyPassword(){
+alert ("senha copiada com sucesso!")
+navigator.clipboard.writeText(novaSenha);
+}
